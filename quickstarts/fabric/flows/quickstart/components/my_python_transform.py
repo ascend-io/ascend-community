7bf98d8c96dca9d8de685f13ac29126ad209c286
Cody Peterson <cody.peterson@ascend.io>
2025-01-31 16:29:13 +0000

    Update files 2025-01-31T16:29:13.704116+00:00

--- quickstarts/fabric/flows/quickstart/components/my_python_transform.py
+++ quickstarts/fabric/flows/quickstart/components/my_python_transform.py
@@ -6,5 +6,4 @@ from ascend.resources import ref, transform
 @transform(inputs=[ref("lake_reader")])
 def my_python_transform(lake_reader: ir.Table, context) -> ir.Table:
   # lake_reader is an Ibis Table object containing records from the lake_reader component
-  t = lake_reader.union(lake_reader)
-  return t
+  return lake_reader
