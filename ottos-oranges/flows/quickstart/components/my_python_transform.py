0320ffcacddaade958d3b1cb16adb3617603ade0
Cody Peterson <cody.peterson@ascend.io>
2025-02-10 11:44:25 -0500

    Update `my_python_transform` to use `azure_reader` instead of `lake_reader` as input reference and variable name.

--- ottos-oranges/flows/quickstart/components/my_python_transform.py
+++ ottos-oranges/flows/quickstart/components/my_python_transform.py
@@ -3,7 +3,7 @@ from ibis import ir
 from ascend.resources import ref, transform
 
 
-@transform(inputs=[ref("lake_reader")])
-def my_python_transform(lake_reader: ir.Table, context) -> ir.Table:
+@transform(inputs=[ref("azure_reader")])
+def my_python_transform(azure_reader: ir.Table, context) -> ir.Table:
   # lake_reader is an Ibis Table object containing records from the lake_reader component
-  return lake_reader
+  return azure_reader
