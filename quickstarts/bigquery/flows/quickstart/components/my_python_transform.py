00e2c4ca050e6e3567010bbc4e13e2702d101634
Sean Knapp <sean@ascend.io>
2025-02-12 14:45:32 -0800

    init

--- quickstarts/bigquery/flows/quickstart/components/my_python_transform.py
+++ quickstarts/bigquery/flows/quickstart/components/my_python_transform.py
@@ -3,11 +3,7 @@ from ibis import ir
 from ascend.resources import ref, transform
 
 
-@transform(
-  inputs=[
-    ref("lake_reader")
-  ]
-)
+@transform(inputs=[ref("lake_reader")])
 def my_python_transform(lake_reader: ir.Table, context) -> ir.Table:
   # lake_reader is an Ibis Table object containing records from the lake_reader component
   return lake_reader
