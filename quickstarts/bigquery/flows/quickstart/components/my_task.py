c28ba67a50c88b9d49460cbc864cf4918f455021
Sean Knapp <sean@ascend.io>
2025-02-12 22:57:11 +0000

    Update files 2025-02-12T22:57:11.437886+00:00

--- quickstarts/bigquery/flows/quickstart/components/my_task.py
+++ quickstarts/bigquery/flows/quickstart/components/my_task.py
@@ -3,8 +3,7 @@ from ascend.resources import ref, task
 
 @task(
     dependencies=[
-        ref("expeditions"),
-        ref("expeditions_partitioned"),
+        ref("expeditions_with_routes"),
     ]
 )
 def my_task(expeditions, context):
