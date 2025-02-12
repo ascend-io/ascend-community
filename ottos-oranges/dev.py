e4362657ac5963f330b215386447a66511ea75c0
Cody <cody@dkdc.dev>
2025-02-12 18:27:38 -0500

    qs

--- ottos-oranges/dev.py
+++ ottos-oranges/dev.py
@@ -16,3 +16,14 @@ ibis.options.repr.interactive.max_depth = 8
 ibis.options.repr.interactive.max_columns = None
 
 faker = Faker()
+
+bootstrap_sql = """
+create secret containername (
+    TYPE AZURE,
+    PROVIDER CONFIG,
+    ACCOUNT_NAME 'codyascend'
+);
+""".strip(";").strip()
+
+
+ibis.get_backend().raw_sql(bootstrap_sql)
