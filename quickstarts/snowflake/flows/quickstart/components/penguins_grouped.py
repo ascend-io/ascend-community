616f936cee978897e872c6a6edf3e103ffc48ce5
Cody Peterson <cody.peterson@ascend.io>
2025-02-13 00:51:24 +0000

    Update files 2025-02-13T00:51:24.360824+00:00

--- quickstarts/snowflake/flows/quickstart/components/penguins_grouped.py
+++ quickstarts/snowflake/flows/quickstart/components/penguins_grouped.py
@@ -1,6 +1,6 @@
 from ascend.resources import ref, transform
 
 @transform(inputs=[ref("penguins")])
-def penguins_grouped(penguins):
+def penguins_grouped(penguins, ctx):
   # add your Transform logic here
   return penguins
