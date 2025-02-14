d6db939ce7fa246c5e5ab77624d345dbe52c2eb1
Cody Peterson <cody.peterson@ascend.io>
2025-02-14 15:33:24 -0500

    Add Ibis import and update `grouped_store` function to group by `store_name` and aggregate counts.

--- ottos-oranges/projects/bigquery-dev/flows/quickstart/components/grouped_store.py
+++ ottos-oranges/projects/bigquery-dev/flows/quickstart/components/grouped_store.py
@@ -1,6 +1,9 @@
+import ibis
+
 from ascend.resources import ref, transform
 
 @transform(inputs=[ref("int_store")])
 def grouped_store(int_store):
   # add your Transform logic here
-  return int_store
+  return int_store.group_by(int_store["store_name"]).agg(count=ibis._.count()).order_by(ibis.desc("count"))
+
