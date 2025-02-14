89201d3eee1eae328a287a9458703a2f6f445c32
Cody <cody@dkdc.dev>
2025-02-14 18:59:51 -0500

    qs

--- ottos-oranges/projects/bigquery-dev/flows/quickstart/components/telemetry_task.py
+++ ottos-oranges/projects/bigquery-dev/flows/quickstart/components/telemetry_task.py
@@ -1,7 +1,8 @@
 from ascend.resources import task, ref
 
+
 @task(dependencies=[ref("int_telemetry")])
 def telemetry_task(int_telemetry, context):
-  # add your Task logic here, you can reference name for this component by using context.component_name
-  for i in range(20):
-    print(i)
+    # add your Task logic here, you can reference name for this component by using context.component_name
+    for i in range(20):
+        print(i)
