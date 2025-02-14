cd63bcb62d45656099df8864cc0fe54a42024171
Cody Peterson <cody.peterson@ascend.io>
2025-02-14 18:54:32 -0500

    Update `telemetry_task` to include a loop that prints numbers from 0 to 19, replacing the previous table creation logic.

--- ottos-oranges/projects/bigquery-dev/flows/quickstart/components/telemetry_task.py
+++ ottos-oranges/projects/bigquery-dev/flows/quickstart/components/telemetry_task.py
@@ -3,4 +3,5 @@ from ascend.resources import task, ref
 @task(dependencies=[ref("int_telemetry")])
 def telemetry_task(int_telemetry, context):
   # add your Task logic here, you can reference name for this component by using context.component_name
-  context.ibis.create_table(context.component_name, int_telemetry, overwrite=True)
+  for i in range(20):
+    print(i)
