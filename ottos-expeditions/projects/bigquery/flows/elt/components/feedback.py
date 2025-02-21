493a2866ee7ccd1b87865876d039730b7404119f
Cody <cody@dkdc.dev>
2025-02-21 11:30:06 -0500

    qs

--- ottos-expeditions/projects/bigquery/flows/elt/components/feedback.py
+++ ottos-expeditions/projects/bigquery/flows/elt/components/feedback.py
@@ -1,3 +1,5 @@
+import ibis
+
 from ascend.resources import ref, transform
 
 
@@ -14,6 +16,22 @@ def feedback(
     feedback_website,
     context,
 ):
-    feedback = feedback_ascenders.union(feedback_stores).union(feedback_website)
+    feedback = (
+        feedback_ascenders.mutate(
+            store_id=ibis.literal(None, type=str), user_id=ibis.literal(None, type=str)
+        )
+        .union(
+            feedback_stores.mutate(
+                ascender_id=ibis.literal(None, type=str),
+                user_id=ibis.literal(None, type=str),
+            )
+        )
+        .union(
+            feedback_website.mutate(
+                ascender_id=ibis.literal(None, type=str),
+                store_id=ibis.literal(0, type=str),
+            )
+        )
+    )
 
     return feedback
