b2941254ae170e5d760cc3c2af4b5f97852daf70
Cody Peterson <cody.peterson@ascend.io>
2025-02-25 22:46:16 +0000

    Update files 2025-02-25T22:46:16.304521+00:00

--- ottos-expeditions/projects/snowflake/flows/elt/components/weather_sensors.py
+++ ottos-expeditions/projects/snowflake/flows/elt/components/weather_sensors.py
@@ -4,7 +4,7 @@ import local_code.transform as T
 from ascend.resources import ref, transform
 
 
-@transform(inputs=[ref("read_weather_sensors", reshape="map")])
+@transform(inputs=[ref("read_weather_sensors")])
 def weather_sensors(read_weather_sensors: ibis.Table, context) -> ibis.Table:
     weather_sensors = T.clean(read_weather_sensors)
     return weather_sensors
