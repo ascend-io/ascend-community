297d9ebcd0b4450fcb0274ff1089f3b263e01d77
Cody <cody@dkdc.dev>
2025-02-25 11:36:15 -0500

    missed at least this

--- ottos-expeditions/projects/snowflake/flows/elt/components/telemetry.py
+++ ottos-expeditions/projects/snowflake/flows/elt/components/telemetry.py
@@ -13,10 +13,10 @@ def telemetry(
     context,
 ):
     telemetry = (
-        telemetry_guides.rename(PERSON_ID="guide_id")
+        telemetry_guides.rename(PERSON_ID="GUIDE_ID")
         .mutate(IS_GUIDE=True, IS_ASCENDER=False)
         .union(
-            telemetry_ascenders.rename(PERSON_ID="ascender_id").mutate(
+            telemetry_ascenders.rename(PERSON_ID="ASCENDER_ID").mutate(
                 IS_GUIDE=False, IS_ASCENDER=True
             )
         )
