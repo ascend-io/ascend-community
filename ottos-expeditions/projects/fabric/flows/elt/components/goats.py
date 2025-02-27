5ca8290ca5e93acff8feeabf328bdfcba60f4f98
Cody <cody@dkdc.dev>
2025-02-27 14:40:03 -0500

    remove telemetry components for now

--- ottos-expeditions/projects/fabric/flows/elt/components/goats.py
+++ ottos-expeditions/projects/fabric/flows/elt/components/goats.py
@@ -5,13 +5,11 @@ from ascend.resources import ref, transform
     inputs=[
         ref("ascenders"),
         ref("routes"),
-        ref("telemetry"),
     ]
 )
 def goats(
     ascenders,
     routes,
-    telemetry,
     context,
 ):
     return ascenders.sample(0.01)
