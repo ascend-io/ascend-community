9bbcc79def0b0a482a5a23270ebbdc759ece3cc2
Sean Knapp <sean@ascend.io>
2025-03-04 17:40:49 +0000

    Rename function `ascenders_analytics` to `ascenders_analytics_v2` in the `ascenders_analytics copy.py` file.

--- ottos-expeditions/projects/bigquery/flows/analytics/components/ascenders_analytics copy.py
+++ ottos-expeditions/projects/bigquery/flows/analytics/components/ascenders_analytics copy.py
@@ -2,6 +2,6 @@ from ascend.resources import ref, transform
 
 
 @transform(inputs=[ref("elt_ascenders", flow="elt")])
-def ascenders_analytics(elt_ascenders, context):
+def ascenders_analytics_v2(elt_ascenders, context):
     ascenders_analytics = elt_ascenders
     return ascenders_analytics
