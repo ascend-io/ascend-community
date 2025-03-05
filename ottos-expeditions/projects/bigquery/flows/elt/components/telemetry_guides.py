f17fe5a41c64bb57db36302fce3622f7b0630625
Sean Knapp <sean@ascend.io>
2025-03-05 02:10:58 +0000

    Refactor `telemetry_guides` function to return the cleaned telemetry guides directly, removing the intermediate variable.

--- ottos-expeditions/projects/bigquery/flows/elt/components/telemetry_guides.py
+++ ottos-expeditions/projects/bigquery/flows/elt/components/telemetry_guides.py
@@ -6,5 +6,4 @@ from ascend.resources import ref, transform
 
 @transform(inputs=[ref("read_telemetry_guides")])
 def telemetry_guides(read_telemetry_guides: ibis.Table, context) -> ibis.Table:
-    telemetry_guides = T.clean(read_telemetry_guides)
-    return telemetry_guides
+    return T.clean(read_telemetry_guides)
