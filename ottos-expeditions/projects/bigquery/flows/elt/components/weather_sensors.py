ec66a209265c91455786991fdf8668efdf1fc7da
Sean Knapp <sean@ascend.io>
2025-03-05 02:10:38 +0000

    Refactor `weather_sensors` function to return the cleaned data directly, removing the intermediate variable.

--- ottos-expeditions/projects/bigquery/flows/elt/components/weather_sensors.py
+++ ottos-expeditions/projects/bigquery/flows/elt/components/weather_sensors.py
@@ -6,5 +6,4 @@ from ascend.resources import ref, transform
 
 @transform(inputs=[ref("read_weather_sensors")])
 def weather_sensors(read_weather_sensors: ibis.Table, context) -> ibis.Table:
-    weather_sensors = T.clean(read_weather_sensors)
-    return weather_sensors
+    return T.clean(read_weather_sensors)
