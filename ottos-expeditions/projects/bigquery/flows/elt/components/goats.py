7a2ae23ffc69eb891845c9329a2fada147a4e2a8
Andrey Siunov <andrey.siunov@ascend.io>
2025-03-06 22:38:00 +0000

    Update files 2025-03-06T22:38:00.858787+00:00

--- ottos-expeditions/projects/bigquery/flows/elt/components/goats.py
+++ ottos-expeditions/projects/bigquery/flows/elt/components/goats.py
@@ -27,22 +27,7 @@ def goats(
         ref("telemetry"),
     ]
 )
-def goats2(
-    ascenders: ibis.Table,
-    routes: ibis.Table,
-    telemetry: ibis.Table,
-    context: ComponentExecutionContext,
-) -> ibis.Table:
-    return ascenders.sample(0.01)
-
-@transform(
-    inputs=[
-        ref("ascenders"),
-        ref("routes"),
-        ref("telemetry"),
-    ]
-)
-def goats3(
+def goats4(
     ascenders: ibis.Table,
     routes: ibis.Table,
     telemetry: ibis.Table,
