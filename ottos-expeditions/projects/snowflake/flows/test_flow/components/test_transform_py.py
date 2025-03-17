2e5e9be09dae387ad062ea05919680936aafd047
Cody Peterson <cody.peterson@ascend.io>
2025-03-17 03:37:13 +0000

    Update files 2025-03-17T03:37:13.121483+00:00

--- ottos-expeditions/projects/snowflake/flows/test_flow/components/test_transform_py.py
+++ ottos-expeditions/projects/snowflake/flows/test_flow/components/test_transform_py.py
@@ -1,6 +1,6 @@
 from ascend.resources import ref, transform
 
 @transform(inputs=[ref("test_read")])
-def test_transform_py(test_read):
+def test_transform_py(test_read, context):
   # add your Transform logic here
   return test_read
